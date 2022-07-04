f9a9518cc1fb2dc8d1e7434d5743c5fabe523bce
choral7451 <chorales@naver.com>
2022-07-04 13:19:40 +0900

    TEST

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  @Get('/')
-  getHello() {
-    return '안녕하세요!';
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TestModule } from './apis/test/test.module';
 import { RedisClientOptions } from 'redis';
 import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -33,5 +34,6 @@ import { AppController } from './app.controller';
     }),
   ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
