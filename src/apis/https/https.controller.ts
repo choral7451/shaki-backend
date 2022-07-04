9db501b0f640f65ec7261b90837c9d7e6979cc2b
choral7451 <chorales@naver.com>
2022-07-04 16:06:34 +0900

    test

--- src/apis/https/https.controller.ts
+++ src/apis/https/https.controller.ts
@@ -3,13 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class HttpsController {
   @Get('/')
-  getHello(): string {
-    console.log('asdsad');
-    return '테스트';
-  }
-  @Get('/test')
-  test(): string {
-    console.log('asdsad');
-    return '테스트';
+  https(): string {
+    return 'https';
   }
 }
